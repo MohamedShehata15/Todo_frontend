268656fc25692a925701308a57274dc886707f50
mohamed159716 <mohamed159716@yahoo.com>
2022-07-10 17:00:12 +0200

    Added React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
    return (
       <div>
-         <Login />
+         <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+         </Routes>
       </div>
    );
 };
