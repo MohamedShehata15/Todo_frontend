f1f28c36df0ca4de03db25ce26936922a826c276
mohamed159716 <mohamed159716@yahoo.com>
2022-07-11 12:05:46 +0200

    Added Protected Route to Home Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ProtectedRoute from "./hooks/ProtectedRoute";
 
 const App = () => {
    return (
@@ -11,7 +12,9 @@ const App = () => {
          <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/" element={<Home />} />
+            <Route path="" element={<ProtectedRoute />}>
+               <Route path="/" element={<Home />} />
+            </Route>
          </Routes>
       </div>
    );
