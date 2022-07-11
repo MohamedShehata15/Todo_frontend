cdc197c49f2ee0a88d6ff3614c97b8a7ef81c692
mohamed159716 <mohamed159716@yahoo.com>
2022-07-11 17:49:28 +0200

    Handled Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
 
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import ProtectedRoute from "./hooks/ProtectedRoute";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
    return (
       <div>
+         <div>
+            <Toaster />
+         </div>
          <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
