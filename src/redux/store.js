8cbe16d9cb1e985894dd996f88c9274bdcd98746
mohamed159716 <mohamed159716@yahoo.com>
2022-07-12 11:55:19 +0200

    Handled Store with user and todo reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
+import todoSlice from "./todoSlice";
 
 export const store = configureStore({
    reducer: {
       user: userSlice,
+      todos: todoSlice,
    },
 });
