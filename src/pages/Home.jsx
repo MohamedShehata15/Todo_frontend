db3149282755ffd67eda9904d866a7f490188ac5
mohamed159716 <mohamed159716@yahoo.com>
2022-07-12 23:27:05 +0200

    Created Header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "../components/Header";
 import DragList from "./../components/DragList";
 
 const Home = () => {
    return (
       <div>
+         <Header />
          <DragList />
       </div>
    );
